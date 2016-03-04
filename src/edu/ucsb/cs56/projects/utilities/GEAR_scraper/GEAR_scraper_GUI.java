e5dd6f6b0f3c785e9d072da202f49c3989497337
Hunter <Hunter@Hunters-Mac-mini.local>
2016-03-03 20:32:54 -0800

    fixed formatting and pulled the procedural button listener into two action listeners

--- src/edu/ucsb/cs56/projects/utilities/GEAR_scraper/GEAR_scraper_GUI.java
+++ src/edu/ucsb/cs56/projects/utilities/GEAR_scraper/GEAR_scraper_GUI.java
@@ -23,31 +23,32 @@ public class GEAR_scraper_GUI implements ItemListener {
     private String[] commands = {"Area D", "Area E", "Area F", "Area G", "Area H", "Special Area Requirements", "Ethnicity", "American Hist Inst", "European", "Writing"};
     private GEAR_scraper scraper;
 
-    private ArrayList<GECourse> allGEs;          // ArrayList of GE Course Objects, pulled directly from the scraper
-    private GECourse[] allGEArray;               // Created from the allGEs ArrayList
+    private ArrayList<GECourse> allGEList;       // ArrayList of GE Course Objects, pulled directly from the scraper
+    private GECourse[] allGEArray;               // Created from the allGEList ArrayList
+    private JList<GECourse> allGEArr;            // This is the scrollable page for all of the GE courses
 
+    
     private ArrayList<GECourse> filteredGEs;     // displayed when the checkboxes are clicked to narrow the search
     private GECourse[] filteredGEList;           // displayed when the checkboxes are clicked to narrow the search
 
-    private JList<GECourse> allGEList;           // This is the scrollable page for all of the GE courses
+    
+    private JPanel checkBoxPanel;                // JPanel for checkboxes
+    private ArrayList<JCheckBox> cboxes;         // ArrayList of checkboxes
 
-    private ArrayList<JCheckBox> cboxes;
-
-    private JPanel checkBoxPanel;
 
     private JFrame frame;
     private JScrollPane scrollPane;              // the scrollable pane that displays the courses
-    private JScrollPane addedCoursePane;
+    private JScrollPane addedCoursePane;         // scollable pane for the courses that the user adds
 
-    private JButton customURLButton;
-    private JButton clearCourses;
-    private JTextField searchBar;
+    private JButton customURLButton;             // Custom url button for the GUI
+    private JButton clearCourses;                // Clear Courses button for the GUI
+    private JTextField searchBar;                // searchBar for the GUI
 
     private ArrayList<String> foundCourseList;   // used to add to the Found Courses List
 
 
-    private ArrayList<String> addedCourseArr; // should only need this to save the searched classes
-    private JList<String> addedCourseList;    // Jlist of the saved coruses
+    private ArrayList<String> addedCourseArr;    // should only need this to save the searched classes
+    private JList<String> addedCourseList;       // Jlist of the saved coruses
 
     private JTextField foundCourses;
 
@@ -55,24 +56,25 @@ public class GEAR_scraper_GUI implements ItemListener {
     private void go() {
 
         //initialize variables
-        MyButtonListener mbl = new MyButtonListener();
+        CustomURLButtonListener customURLBL = new CustomURLButtonListener();
         MySearchBarListener msbl = new MySearchBarListener();
+        ClearCourseScheduleListener clearCourseScheduleBL = new ClearCourseScheduleListener();
         customURLButton = new JButton("Custom URL");
-        scraper = new GEAR_scraper();                              // uses the default url
-        allGEs = scraper.createArrayList();                        // allGEs is an ArrayList of GECourse objects
-        allGEArray = new GECourse[allGEs.size()];
-		allGEArray = allGEs.toArray(allGEArray);                   // use allGEs to create an array, allGEArray
+        scraper = new GEAR_scraper();                                 // uses the default url
+        allGEList = scraper.createArrayList();                        // allGEList is an ArrayList of GECourse objects
+        allGEArray = new GECourse[allGEList.size()];
+		allGEArray = allGEList.toArray(allGEArray);                   // use allGEList to create an array, allGEArray
 
-        filteredGEs = new ArrayList<GECourse>(allGEs);             // used in the show method and the searchBar Listener
+        filteredGEs = new ArrayList<GECourse>(allGEList);             // used in the show method and the searchBar Listener
 
         searchBar = new JTextField("Search for a Course: ", 20);
         foundCourses = new JTextField("Found Courses: ", 20);
         foundCourseList = new ArrayList<String>();
         clearCourses = new JButton("Clear Course Schedule");
-        clearCourses.setActionCommand("clear");
+        //clearCourses.setActionCommand("clear");
 
         // A graphical list of GECouse objects
-        allGEList = new JList<GECourse>(allGEArray);
+        allGEArr = new JList<GECourse>(allGEArray);
 
 
         // USed for the saved Course JList
@@ -101,7 +103,7 @@ public class GEAR_scraper_GUI implements ItemListener {
 
 
         //GUI CRAP
-        customURLButton.setActionCommand("url");
+        //customURLButton.setActionCommand("url");
         checkBoxPanel.setLayout(new BoxLayout(checkBoxPanel, BoxLayout.Y_AXIS));
         for (String cmd : commands) {
             temp = new JCheckBox(cmd);
@@ -113,13 +115,13 @@ public class GEAR_scraper_GUI implements ItemListener {
 
         // set up everything to add to the frame
 
-        customURLButton.addActionListener(mbl);
+        customURLButton.addActionListener(customURLBL);        // custom URL Button Listener
         checkBoxPanel.add(customURLButton);
 
-        searchBar.addActionListener(msbl);   // my search bar listener
-        clearCourses.addActionListener(mbl); // my button listener
+        searchBar.addActionListener(msbl);                     // my search bar listener
+        clearCourses.addActionListener(clearCourseScheduleBL); // clear Course Schedule Button Listener
 
-        scrollPane = new JScrollPane(allGEList);
+        scrollPane = new JScrollPane(allGEArr);
         scrollPane.setPreferredSize(new Dimension(250, 500));
 
 
@@ -134,7 +136,7 @@ public class GEAR_scraper_GUI implements ItemListener {
     }
 
 
-    /* reacts to checkboxes being clicked 
+    /* reacts to checkboxes being clicked
      */
 
     public void itemStateChanged(ItemEvent e) {
@@ -192,7 +194,7 @@ public class GEAR_scraper_GUI implements ItemListener {
             // removes the scrollapne from the jpanel
             checkBoxPanel.remove(addedCoursePane);
             addedCoursePane = new JScrollPane(addedCourseList);
-            scrollPane.setPreferredSize(new Dimension(100, 100));
+            scrollPane.setPreferredSize(new Dimension(250, 500));
             checkBoxPanel.add(addedCoursePane, 1); // want to add it back into the same place, index 1
 
             frame.invalidate();
@@ -203,75 +205,68 @@ public class GEAR_scraper_GUI implements ItemListener {
     }
 
 
+    /* reacts to Clear Course Schedule Button being pressed
+
+    */
+private class ClearCourseScheduleListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            foundCourseList.clear();
+            addedCourseArr.clear();
+
+            // remakes the entire addedCoursesPane
+
+            // need to clear the addedCoursesList and then update the JList
+            addedCourseList = new JList<String>(addedCourseArr.toArray(new String[addedCourseArr.size()]));
+
+            // removes the old addedCoursePane from the frame, makes a new one and then adds it back
+            addedCoursePane.setVisible(false);
+            // removes the scrollapne from the jpanel
+            checkBoxPanel.remove(addedCoursePane);
+            addedCoursePane = new JScrollPane(addedCourseList);
+            scrollPane.setPreferredSize(new Dimension(250, 500));
+            checkBoxPanel.add(addedCoursePane, 1); // want to add it back into the same place, index 1
+
+            frame.invalidate();
+            frame.validate();
+        }
+
+    }
 
 
 
-    /* reacts to Buttons being pressed, this can be fixed as it is a procedural action listener
-    Would be better to implement another one for the Clear Courses button separately
+    /* reacts to Custom URL Button being pressed
      */
-    private class MyButtonListener implements ActionListener {
+    private class CustomURLButtonListener implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
-            String action = e.getActionCommand();
-            switch (action) {
+            String line;
+            String start;
+            String end;
+            int startPage;
+            int endPage;
+            URL url;
+            line = JOptionPane.showInputDialog("Enter the URL");
+            try {
+                url = new URL(line);
+            } catch (MalformedURLException ex) {
 
-                case "url": // CODE SMELL
-
-                    String line;
-                    String start;
-                    String end;
-                    int startPage;
-                    int endPage;
-                    URL url;
-                    line = JOptionPane.showInputDialog("Enter the URL");
-                    try {
-                        url = new URL(line);
-                    } catch (MalformedURLException ex) {
-
-                        JOptionPane.showMessageDialog(null, "There was an error following the URL.");
-                        return;
-                    }
-                    start = JOptionPane.showInputDialog("Enter the page in GEAR the GE's start on");
-                    end = JOptionPane.showInputDialog("Enter the page in GEAR the GE's end on");
-                    try {
-                        startPage = Integer.parseInt(start);
-                        endPage = Integer.parseInt(end);
-                        if (endPage - startPage > 10)
-                            throw new NumberFormatException();
-                    } catch (NumberFormatException ex) {
-                        JOptionPane.showMessageDialog(null, "Those weren't properly formatted numbers. Make sure to only put the page numbers the GE's are on.");
-                        return;
-                    }
-                    scraper = new GEAR_scraper(url, startPage, endPage);
-                    allGEs = scraper.createArrayList();
-                    show("all");
-
-
-                case "clear": // CODE SMELL
-                    // will need to be adapted for the scrollable text box
-
-                    //foundCourses.setText("Found Courses: ");
-                    foundCourseList.clear();
-                    addedCourseArr.clear();
-
-                    // remakes the entire addedCoursesPane
-
-                    // need to clear the addedCoursesList and then update the JList
-                    addedCourseList = new JList<String>(addedCourseArr.toArray(new String[addedCourseArr.size()]));
-
-                    // removes the old addedCoursePane from the frame, makes a new one and then adds it back
-                    addedCoursePane.setVisible(false);
-                    // removes the scrollapne from the jpanel
-                    checkBoxPanel.remove(addedCoursePane);
-                    addedCoursePane = new JScrollPane(addedCourseList);
-                    scrollPane.setPreferredSize(new Dimension(100, 100));
-                    checkBoxPanel.add(addedCoursePane, 1); // want to add it back into the same place, index 1
-
-                    frame.invalidate();
-                    frame.validate();
-
-
+                JOptionPane.showMessageDialog(null, "There was an error following the URL.");
+                return;
             }
+            start = JOptionPane.showInputDialog("Enter the page in GEAR the GE's start on");
+            end = JOptionPane.showInputDialog("Enter the page in GEAR the GE's end on");
+            try {
+                startPage = Integer.parseInt(start);
+                endPage = Integer.parseInt(end);
+                if (endPage - startPage > 10)
+                    throw new NumberFormatException();
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Those weren't properly formatted numbers. Make sure to only put the page numbers the GE's are on.");
+                return;
+            }
+            scraper = new GEAR_scraper(url, startPage, endPage);
+            allGEList = scraper.createArrayList();
+            show("all");
 
         }
     }
@@ -284,7 +279,7 @@ public class GEAR_scraper_GUI implements ItemListener {
 
         GECourse a;
         // resets the filteredGEs array on every checkbox clicked to update the scrollPane
-        filteredGEs = new ArrayList<GECourse>(allGEs);
+        filteredGEs = new ArrayList<GECourse>(allGEList);
 
         for (int i = filteredGEs.size() - 1; i >= 0; i--) {
             a = filteredGEs.get(i);
@@ -361,12 +356,12 @@ public class GEAR_scraper_GUI implements ItemListener {
         allGEArray = filteredGEs.toArray(allGEArray);
 
         // creates the new JList
-        allGEList = new JList<GECourse>(allGEArray);
+        allGEArr = new JList<GECourse>(allGEArray);
 
         // removes the JList from the frame, makes a new one and then adds it back
         scrollPane.setVisible(false);
         frame.getContentPane().remove(scrollPane);
-        scrollPane = new JScrollPane(allGEList);
+        scrollPane = new JScrollPane(allGEArr);
         scrollPane.setPreferredSize(new Dimension(250, 500));
         frame.getContentPane().add(BorderLayout.EAST, scrollPane);
 
